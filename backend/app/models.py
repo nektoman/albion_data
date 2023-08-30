c5a7ce277da8afcdc212b39848e1c9f63b7f65b4
Nikita <kuvalda008@gmail.com>
2023-08-30 14:48:11 +0300

    Implement main db tables

--- backend/app/models.py
+++ backend/app/models.py
@@ -27,7 +27,7 @@ class Quality(db.Model):
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String)
-    
+
     def __repr__(self):
         return '<Location{})>'.format(self.text)
 
@@ -49,24 +49,28 @@ class Item(db.Model):
         return '<Item{})>'.format(self.text)
 
 
-class MediumPrice(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class SalesByDay(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     item_id = db.Column(db.Integer, db.ForeignKey("item.id"))
     tier = db.Column(db.Integer, db.ForeignKey("tier.tier"))
     enhancement = db.Column(db.Integer, db.ForeignKey("enhancement.enh_tier"))
     quality = db.Column(db.Integer, db.ForeignKey("quality.qual_tier"))
-    price = db.Column(db.Integer)
-    datetime = db.Column(db.Float, nullable=False)
     location = db.Column(db.Integer, db.ForeignKey("location.location"))
+    price = db.Column(db.Integer)
+    sold = db.Column(db.Integer)
+    sold_date = db.Column(db.Float, nullable=False)
+    update_date = db.Column(db.Float, nullable=False)
 
-    def __init__(self, item_id, tier, enhancement, quality, price, location):
+    def __init__(self, item_id, tier, enhancement, quality, location, price, sold, sold_date):
         self.item_id = item_id
         self.tier = tier
         self.enhancement = enhancement
         self.quality = quality
-        self.price = price
         self.location = location
-        self.datetime = time.time()
+        self.price = price
+        self.sold = sold
+        self.sold_date = sold_date
+        self.update_date = time.time()  # todo date
 
     def __repr__(self):
-        return '<MediumPrice todo {})>'.format(self.price)
+        return f'<SalesByDay {self.id})>'
