9f15f268b6e84662ac69cae3668d65beaeba1820
Nikita <kuvalda008@gmail.com>
2023-08-30 23:36:13 +0300

    Create init alembic migration

---
+++ backend/migrations/versions/71986b78f002_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 71986b78f002
+Revises: 
+Create Date: 2023-08-30 19:34:38.868902
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '71986b78f002'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
